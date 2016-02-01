60ec66982ba2086144df4d4506b0706132d2b45b
Markus Hjort <markus.hjort@gmail.com>
2016-02-01 18:12:20 +0200

    Bumb clj-gatling

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]
-                 [clj-gatling "0.7.5"]
+                 [clj-gatling "0.7.6"]
                  [uswitch/lambada "0.1.0"]
                  [amazonica "0.3.48" :exclusions [com.amazonaws/aws-java-sdk]]
                  [com.amazonaws/aws-java-sdk-lambda "1.10.49"]

1ae11e9ea20a99604877fe58e3c5b9966208b30f
Markus Hjort <markus.hjort@gmail.com>
2016-02-01 21:30:35 +0200

    Release version 0.1.3 with new clj-gatling

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clojider "0.1.2"
+(defproject clojider "0.1.3"
   :description "FIXME: write description"
   :url "https://github.com/mhjort/clojider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]
-                 [clj-gatling "0.7.6"]
+                 [clj-gatling "0.7.7"]
                  [uswitch/lambada "0.1.0"]
                  [com.amazonaws/aws-java-sdk-lambda "1.10.49"]
                  [com.amazonaws/aws-java-sdk-core "1.10.49"]

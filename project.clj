687b8b93a1775f173ed01b7b9b03b41951d08eaf
Markus Hjort <markus.hjort@gmail.com>
2016-02-01 18:13:12 +0200

    Release first version 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojider "0.1.0-SNAPSHOT"
+(defproject clojider "0.1.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mhjort/clojider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]

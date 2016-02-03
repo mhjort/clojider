b289f3851f4045c8ca605e7030f8498fe1067f11
Markus Hjort <markus.hjort@gmail.com>
2016-02-03 21:26:24 +0200

    Release version 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojider "0.1.4"
+(defproject clojider "0.1.5"
   :description "FIXME: write description"
   :url "https://github.com/mhjort/clojider"
   :license {:name "Eclipse Public License"

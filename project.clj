af815e6d2fed72d9d7d05bb5393648195fd853e1
Markus Hjort <markus.hjort@gmail.com>
2016-02-09 19:47:40 +0200

    Bump AWS SDK

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
                  [cheshire "5.5.0"]
                  [clj-gatling "0.7.8"]
                  [uswitch/lambada "0.1.0"]
-                 [com.amazonaws/aws-java-sdk-iam "1.10.49"]
-                 [com.amazonaws/aws-java-sdk-lambda "1.10.49"]
-                 [com.amazonaws/aws-java-sdk-core "1.10.49"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.10.49"]]
+                 [com.amazonaws/aws-java-sdk-iam "1.10.50"]
+                 [com.amazonaws/aws-java-sdk-lambda "1.10.50"]
+                 [com.amazonaws/aws-java-sdk-core "1.10.50"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.10.50"]]
   :uberjar-exclusions [#"scala.*"]
   :aot [clojider.lambda])
 

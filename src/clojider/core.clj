9f20e46d2a080f5d6e1105fa365f2256c1ab568e
Markus Hjort <markus.hjort@gmail.com>
2016-02-09 21:51:50 +0200

    Add load-lambda and load-local commands

--- src/clojider/core.clj
+++ src/clojider/core.clj
@@ -1,16 +1,43 @@
 (ns clojider.core
   (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojider.rc :as rc]
+            [clojider.utils :refer [symbol-namespace]]
+            [clj-gatling.core :as gatling]
+            [clj-time.core :as t]
             [clojider.aws :as aws])
   (:gen-class))
 
 (def cli-options
   [["-r" "--region REGION" "Which region to use"]
-   ["-f" "--file FILE" "Path of uberjar"]])
+   ["-f" "--file FILE" "Path of uberjar"]
+   ["-s" "--simulation SIMULATION" "Fully qualified name of simulation"]
+   ["-c" "--concurrency CONCURRENCY" "Concurrency"
+    :default 1
+    :parse-fn #(Integer/parseInt %)]
+   ["-d" "--duration DURATION" "Duration in seconds"
+    :default (t/seconds 1)
+    :parse-fn #(t/seconds (Integer/parseInt %))]])
+
+(defn run-with-lambda [{:keys [simulation region concurrency duration]}]
+  (rc/run-simulation (symbol simulation) {:region region
+                                          :concurrency concurrency
+                                          :bucket-name (str "clojider-results-" region)
+                                          :duration duration}))
+
+
+(defn run-using-local-machine [{:keys [simulation concurrency duration]}]
+  (load (symbol-namespace simulation))
+  (gatling/run-simulation (eval (read-string simulation))
+                          concurrency
+                          {:root "tmp"
+                           :duration duration}))
 
 (def cmds
   {"install" aws/install-lambda
    "uninstall" aws/uninstall-lambda
-   "update" aws/update-lambda})
+   "update" aws/update-lambda
+   "load-lambda" run-with-lambda
+   "load-local" run-using-local-machine})
 
 (defn -main [& args]
   (let [options (parse-opts args cli-options)

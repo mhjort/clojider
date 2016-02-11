5e76b50658c641a270778b4e60a144f6bc24d0a1
Markus Hjort <markus.hjort@gmail.com>
2016-02-11 15:43:35 +0200

    Add proper examples

--- src/clojider/examples.clj
+++ src/clojider/examples.clj
@@ -1,5 +1,36 @@
-(ns clojider.examples)
+(ns clojider.examples
+  (:require [org.httpkit.client :as http]))
+
+(def base-url "http://clj-gatling-demo-server.herokuapp.com")
+
+(defn- http-get [url callback _]
+  (let [check-status (fn [{:keys [status]}] (callback (= 200 status)))]
+    (http/get (str base-url url) {} check-status)))
+
+(defn- http-get-with-ids [url ids callback {:keys [user-id]}]
+  (let [check-status (fn [{:keys [status]}] (callback (= 200 status)))
+        id (nth ids user-id)]
+    (http/get (str base-url url id) {} check-status)))
+
+(def ping
+  (partial http-get "/ping"))
 
 (def ping-simulation
-  [{:name "Test"
-   :requests [{:name "ping" :http "http://clj-gatling-demo-server.herokuapp.com/ping"}]}])
+  [{:name "Ping scenario"
+    :requests [{:name "Ping Endpoint" :fn ping}]}])
+
+(def article-read
+  (partial http-get-with-ids "/metrics/article/read/" (cycle (range 100 200))))
+
+(def program-start
+  (partial http-get-with-ids "/metrics/program/start/" (cycle (range 200 400))))
+
+(def metrics-simulation
+  [{:name "Article read scenario"
+    :weight 2
+    :requests [{:name "Article read request"
+                :fn article-read}]}
+   {:name "Program start scenario"
+    :weight 1
+    :requests [{:name "Program start request"
+                :fn program-start}]}])

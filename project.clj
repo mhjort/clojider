3eae7c35c8421fcc672f181e017f9f79c96a4dd7
Markus Hjort <markus.hjort@gmail.com>
2016-02-11 21:14:34 +0200

    Release version 0.2.2 with new clj-gatling version
    Now local and Lambda run both will generate results to folder
    with timestamp in its' name.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clojider "0.2.1"
+(defproject clojider "0.2.2"
   :description "AWS Lambda powered, distributed load testing tool for Clojure"
   :url "https://github.com/mhjort/clojider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cheshire "5.5.0"]
-                 [clj-gatling "0.7.8"]
+                 [clj-gatling "0.7.9"]
                  [uswitch/lambada "0.1.0"]
                  [org.clojure/tools.cli "0.3.3"]
                  [com.amazonaws/aws-java-sdk-iam "1.10.50"]

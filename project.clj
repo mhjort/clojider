b6b52fadb5291ba93d0ac9e6eed0c5973c858dd2
Markus Hjort <markus.hjort@gmail.com>
2016-08-15 20:35:02 +0300

    Release verion 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojider "0.3.0"
+(defproject clojider "0.4.0"
   :description "AWS Lambda powered, distributed load testing tool for Clojure"
   :url "https://github.com/mhjort/clojider"
   :license {:name "Eclipse Public License"

4cdac00f75e1b832c9e051defb875afe74660e20
Markus Hjort <markus.hjort@gmail.com>
2016-08-15 20:26:27 +0300

    Bump clojure 1.7 => 1.8

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mhjort/clojider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.5.0"]
                  [clj-gatling "0.7.9"]
                  [uswitch/lambada "0.1.0"]

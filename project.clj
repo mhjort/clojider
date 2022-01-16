4314dbbd5304b9c6f5e86da850f7a92a5042e4b0
Markus Hjort <markus.hjort@gmail.com>
2022-01-16 20:14:31 +0200

    Bump Clojure

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/mhjort/clojider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [cheshire "5.5.0"]
                  [clj-gatling "0.12.0"]
                  [clojider-gatling-highcharts-s3-reporter "0.2.0"]
-                 [uswitch/lambada "0.1.0"]
+                 [uswitch/lambada "0.1.2"]
                  [org.clojure/tools.cli "0.3.7"]
                  [com.amazonaws/aws-java-sdk-iam "1.10.50"]
                  [com.amazonaws/aws-java-sdk-lambda "1.10.50"]
